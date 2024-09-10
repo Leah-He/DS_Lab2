8de374b3018d6ba507f095fa3cc0b3d54bcda8eb
chenglan.he <chenglan.he@10.14.1.78>
2024-09-10 10:11:17 -0400

    draft2

--- src/main/java/MultiDayPerWeekEvent.java
+++ src/main/java/MultiDayPerWeekEvent.java
@@ -3,22 +3,44 @@ import java.util.GregorianCalendar;
 import calendar.MeetingCalendar;
 
 public class MultiDayPerWeekEvent extends CalendarEvent{
+	private GregorianCalendar repeatUntil;
+	private int[] days;
 
 	/**
 	 * @param description
 	 * @param location
 	 * @param startTime
 	 * @param endTime
+	 * @param repeatUntil
+	 * @param days
 	 */
 	public MultiDayPerWeekEvent(String description, String location, GregorianCalendar startTime,
-			GregorianCalendar endTime) {
+			GregorianCalendar endTime, GregorianCalendar repeatUntil, int[] days) {
 		super(description, location, startTime, endTime);
+		this.repeatUntil = repeatUntil;
+		this.days = days;
 	}
 
 	@Override
 	public void sheduleEvent(MeetingCalendar calendar) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public GregorianCalendar getRepeatUntil() {
+		return repeatUntil;
+	}
+
+	public void setRepeatUntil(GregorianCalendar repeatUntil) {
+		this.repeatUntil = repeatUntil;
+	}
+
+	public int[] getDays() {
+		return days;
+	}
+
+	public void setDays(int[] days) {
+		this.days = days;
 	}
 
 }
--- src/main/java/WeeklyEvent.java
+++ src/main/java/WeeklyEvent.java
@@ -3,21 +3,33 @@ import java.util.GregorianCalendar;
 import calendar.MeetingCalendar;
 
 public class WeeklyEvent extends CalendarEvent{
+	private GregorianCalendar repeatUntil;
 
 	/**
 	 * @param description
 	 * @param location
 	 * @param startTime
 	 * @param endTime
+	 * @param repeatUntil
 	 */
-	public WeeklyEvent(String description, String location, GregorianCalendar startTime, GregorianCalendar endTime) {
+	public WeeklyEvent(String description, String location, GregorianCalendar startTime, GregorianCalendar endTime,
+			GregorianCalendar repeatUntil) {
 		super(description, location, startTime, endTime);
+		this.setRepeatUntil(repeatUntil);
 	}
 
 	@Override
 	public void sheduleEvent(MeetingCalendar calendar) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public GregorianCalendar getRepeatUntil() {
+		return repeatUntil;
+	}
+
+	public void setRepeatUntil(GregorianCalendar repeatUntil) {
+		this.repeatUntil = repeatUntil;
 	}
 
 }
