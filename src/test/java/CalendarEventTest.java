ec4dffddf0811d905ae1c1b40d9867e8b0d95099
chenglan.he <chenglan.he@10.14.1.78>
2024-09-12 10:39:43 -0400

    11

--- src/test/java/CalendarEventTest.java
+++ src/test/java/CalendarEventTest.java
@@ -4,29 +4,20 @@
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.GregorianCalendar;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import calendar.Meeting;
-import calendar.MeetingCalendar;
 
 /**
  * 
  */
 class CalendarEventTest {
 
-
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
-		
 	}
-
-	
 
 	/**
 	 * Test method for {@link CalendarEvent#CalendarEvent(java.lang.String, java.lang.String, java.util.GregorianCalendar, java.util.GregorianCalendar)}.
